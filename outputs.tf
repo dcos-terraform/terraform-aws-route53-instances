3846add0c63243b79214f619be36bab93d448d6d
Weston Bassler <wbassler@mesosphere.com>
2018-11-05 12:22:24 -0500

    updated outputs value to default list. Users can choose how to sort using locals if needed

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "public_fqdns" {
   description = "List of Public FQDNs"
-  value       = "${join("\n", aws_route53_record.instance_public.*.name)}"
+  value       = ["${aws_route53_record.instance_public.*.name}"]
 }
 
 output "private_fqdns" {
   description = "List of Private FQDNs"
-  value       = "${join("\n", aws_route53_record.instance_internal.*.name)}"
+  value       = ["${aws_route53_record.instance_internal.*.name}"]
 }
 
 output "private_ips" {
   description = "List of Private IPs"
-  value       = "${join("\n  - ", flatten(list(aws_route53_record.instance_public.*.records)))}"
+  value       = ["${aws_route53_record.instance_public.*.records}"]
 }
 
 output "public_ips" {
   description = "List of Public IPs"
-  value       = "${join("\n  - ", flatten(list(aws_route53_record.instance_internal.*.records)))}"
+  value       = ["${aws_route53_record.instance_internal.*.records}"]
 }
