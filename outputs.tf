3e33920592250f564e0466747b10a767203a996b
Weston Bassler <wbassler@mesosphere.com>
2018-11-05 12:43:49 -0500

    mixed public and private ips in outputs

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,10 @@ output "private_fqdns" {
 
 output "private_ips" {
   description = "List of Private IPs"
-  value       = ["${aws_route53_record.instance_public.*.records}"]
+  value       = ["${aws_route53_record.instance_internal.*.records}"]
 }
 
 output "public_ips" {
   description = "List of Public IPs"
-  value       = ["${aws_route53_record.instance_internal.*.records}"]
+  value       = ["${aws_route53_record.instance_public.*.records}"]
 }
